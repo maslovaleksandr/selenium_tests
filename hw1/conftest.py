896b5f31d71925c83a715ea6248266cc4877ceb5
Aleksandr Maslov <a.mslv@outlook.com>
2020-07-25 19:31:59 +0300

    test for chrome/firefox

--- hw1/conftest.py
+++ hw1/conftest.py
@@ -7,8 +7,19 @@ def pytest_addoption(parser):
         action='store',
         default='chrome',
         help='Chose browser you want to use. Chrome or FireFox')
+    parser.addoption(
+        "--url",
+        action='store',
+        default="http://192.168.0.102/index.php",
+        help='URL'
+    )
 
 
 @pytest.fixture
 def browser_check(request):
     return request.config.getoption("--b")
+
+
+@pytest.fixture
+def url_check(request):
+    return request.config.getoption("--url")
--- hw1/test_browsers.py
+++ hw1/test_browsers.py
@@ -1,11 +1,19 @@
-import selenium.webdriver
+from selenium import webdriver
+from selenium.webdriver import FirefoxOptions, ChromeOptions
 
 
-def test_browser(browser_check):
+def test_browser(browser_check, url_check):
     if browser_check == "chrome":
-        return selenium.webdriver.Chrome()
+        options = ChromeOptions()
+        options.add_argument("--kiosk")
+        wd = webdriver.Chrome(chrome_options=options)
+        wd.get(url_check)
+        wd.quit()
     elif browser_check == "firefox":
-        return selenium.webdriver.Firefox()
-    else:
-        print("Wrong param")
+        options = FirefoxOptions()
+        options.add_argument("--kiosk")
+        wd = webdriver.Firefox(firefox_options=options)
+        wd.get(url_check)
+        wd.quit()
+
 
