7d6731619f4f580b2dfbcf721d5155bea73df666
Aleksandr Maslov <a.mslv@outlook.com>
2020-08-03 23:44:41 +0300

    added new funcs

--- test_page_object/page_objects/UserPage.py
+++ test_page_object/page_objects/UserPage.py
@@ -4,8 +4,15 @@ from ..locators.Common import Common
 
 class UserPage(DefaultPage):
 
-    def login_user(self, user, password):
-        self._input(Common.user_login.email_input, user)
+    def login_user(self, email, password):
+        self._input(Common.user_login.email_input, email)
         self._input(Common.user_login.password_input, password)
         self._click(Common.user_login.submit)
         return self
+    
+    def login_admin(self, username, password):
+        self._input(Common.user_login.username_input, username)
+        self._input(Common.user_login.password_input, password)
+        self._click(Common.user_login.submit)
+        return self
+    
--- test_page_object/page_objects/__init__.py
+++ test_page_object/page_objects/__init__.py
@@ -1,3 +1,4 @@
 from .MainPage import MainPage
 from .ProductPage import ProductPage
 from .UserPage import UserPage
+from .AdminPanel import AdminPanel
