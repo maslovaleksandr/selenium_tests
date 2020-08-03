70f9c02f876110f1c2d738512282ff38c9ecd3c1
Aleksandr Maslov <a.mslv@outlook.com>
2020-08-03 23:48:39 +0300

    hot fix

--- test_page_object/test_login/test_login.py
+++ test_page_object/test_login/test_login.py
@@ -2,7 +2,6 @@ from ..page_objects import MainPage, UserPage
 
 
 def test_login(browser):
-    MainPage(browser)\
-        .login.click_login()
+    MainPage(browser).login.click_login()
     UserPage(browser).login_user("test@test.test", "test")
-
+    
