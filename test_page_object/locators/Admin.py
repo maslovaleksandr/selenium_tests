87e68f4e39c4162f32983cb7d77b0e26b9fd8ad9
Aleksandr Maslov <a.mslv@outlook.com>
2020-08-03 23:42:09 +0300

    locators for admin panel

---
+++ test_page_object/locators/Admin.py
@@ -0,0 +1,6 @@
+class Admin:
+    catalog = {'css': "#menu-catalog"}
+    downloads = {'css': "#collapse1 > li:nth-child(8) > a:nth-child(1)"}
+    plus_button = {'css': '[data-original-title="Add New"]'}
+    input_file = {'css': "#form-upload input"}
+    upload_button = {'css': "#button-upload"}
